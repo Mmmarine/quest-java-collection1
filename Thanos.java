4a5a8a3a5e8ed7ccfed14f6d6e72ad32b07cc6d2
Marine Mombereau <Marinemombereau@gmail.com>
2019-10-10 10:16:57 +0200

    first commit

--- Thanos.java
+++ Thanos.java
@@ -3,26 +3,44 @@ import java.util.*;
 public class Thanos {
 
     public static void main(String[] args) {
+        Hero Hulk = new Hero ("Hulk", 49);
+        Hero Thor = new Hero ("Thor", 1500);
+        Hero ScarletWitch = new Hero ("Scarlet Witch", 29);
+        Hero IronMan = new Hero ("Iron Man", 48);
+        Hero Vision = new Hero ("Vision", 3);
+        Hero CaptainAmerica = new Hero ("Captain america", 100);
+        Hero BlackWidow = new Hero ("Black Widow", 34);
+        Hero SpiderMan = new Hero ("Spider-Man", 18);
+        Hero DoctorStrange = new Hero ("Doctor Strange", 42);
 
         // TODO 1 : Create an empty heroes list
-
+        ArrayList<Hero> heroes = new ArrayList<>();
         // TODO 2 : Add those heroes to the list
-        // name: Black Widow, age: 34
-        // name: Captain America, age: 100
-        // name: Vision, age: 3
-        // name: Iron Man, age: 48
-        // name: Scarlet Witch, age: 29
-        // name: Thor, age: 1500
-        // name: Spider-Man, age: 18
-        // name: Hulk, age: 49
-        // name: Doctor Strange, age: 42
+
+        heroes.add(BlackWidow);
+        heroes.add(CaptainAmerica);
+        heroes.add(Vision);
+        heroes.add(IronMan);
+        heroes.add(ScarletWitch);
+        heroes.add(Thor);
+        heroes.add(SpiderMan);
+        heroes.add(Hulk);
+        heroes.add(DoctorStrange);
 
         // TODO 3 : It's Thor birthday, now he's 1501
+        Hero Thor2 = new Hero ("Thor", 1501);
+        heroes.set(5, Thor2);
 
         // TODO 4 : Shuffle the heroes list
+        Collections.shuffle(heroes);
 
         // TODO 5 : Keep only the half of the list
+        List<Hero> heroes2 = heroes.subList(0, 4);
 
         // TODO 6 : Loop throught the list and display the name of the remaining heroes
+        for ( int i = 0; i < heroes2.size(); i++) {
+            Hero name = heroes2.get(i);
+            System.out.println(name.getName());
+        }
     }
 }
